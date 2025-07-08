337dd3fe8c7b534f60aa3a52cd5e4244f7a559d7
ItsXete <[dgiabach2000@gmail.com]>
2025-07-08 12:48:10 -0400

    url shortening and random number gen

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -17,7 +19,21 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: longURL };
   res.render("urls_show", templateVars);
+});
+
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8);
+}
+
+app.post("/urls", (req, res) => {
+  const id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`);
 });
